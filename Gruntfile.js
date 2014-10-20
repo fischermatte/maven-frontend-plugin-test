6f174c6732e3612e02cf307722cc6694f9ebd1a3
fischermatte <fischermatte@posteo.net>
2014-10-20 09:28:06 +0200

    fixed extend expression

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,10 @@ module.exports = function (grunt) {
     var buildConfig = {
         pkg: grunt.file.readJSON("package.json")
     };
-    buildConfig = grunt.util.extend(buildConfig, require('./grunt/variables.js'));
+    buildConfig = grunt.util._.extend(buildConfig, require('./grunt/variables.js'));
     var taskConfig = require('load-grunt-configs')(grunt, buildConfig);
 
-    grunt.initConfig(grunt.util.extend(taskConfig, buildConfig));
+    grunt.initConfig(grunt.util._.extend(taskConfig, buildConfig));
 
     grunt.registerTask('build', [
         'clean'
